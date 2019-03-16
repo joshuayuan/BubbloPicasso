7b61a9cdc62c80b182000741421819845918ec22
Joshua Yuan <joshua43210@gmail.com>
2019-03-16 15:41:36 -0700

    add palm position

--- leap-client/client.js
+++ leap-client/client.js
@@ -17,13 +17,38 @@ controller.on("deviceStopped", () => console.log("A Leap device has been disconn
 controller.connect();
 
 var numberOfFingers = 0;
+var palmPosition = [0, 0, 0]; // [x, y, z]
 controller.on('deviceFrame', function(frame) {
   var currNumFingers = frame.fingers.length;
   if (numberOfFingers != currNumFingers) {
     numberOfFingers = currNumFingers;
-    socket.emit("client:server", numberOfFingers);
+    socket.emit("client:server", {"num_fingers": numberOfFingers});
     console.log(numberOfFingers);
+  }
+
+  if (frame.hands.length > 0) {
+    for (var i = 0; i < frame.hands.length; i++) {
+      var hand = frame.hands[i];
+      var currPalmPosition = hand.palmPosition;
+      if (vectorDist(currPalmPosition, palmPosition) > 5) {
+        palmPosition = currPalmPosition;
+        console.log(currPalmPosition);
+        socket.emit("client:server", {"hand_pos": vectorToString(palmPosition)});
+      }
+    }
   }
 });
 
+function vectorDist(v1, v2) {
+  return Math.sqrt(Math.pow(v1[0] - v2[0], 2) + Math.pow(v1[1] - v2[1], 2) + Math.pow(v1[2] - v2[2], 2));
+};
+
+function vectorToString(vector, digits) {
+  if (typeof digits === "undefined") {
+    digits = 1;
+  }
+  return "(" + vector[0].toFixed(digits) + ", "
+             + vector[1].toFixed(digits) + ", "
+             + vector[2].toFixed(digits) + ")";
+};
 
