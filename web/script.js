db9cabd4f3b7651e12c28a16ff3218fd08f32d90
Joshua <joshua43210@gmail.com>
2019-03-16 23:00:17 +0000

    add server side palm position handling

--- web/script.js
+++ web/script.js
@@ -1,4 +1,6 @@
 var socket = io();
 socket.on("server:website", (data) => {
-  $("#num_fingers").text(data);
+  $("#num_fingers").text(data["num_fingers"]);
+  $("#hand_pos").text(data["hand_pos"]);
 });
+
