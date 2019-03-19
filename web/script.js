343863224e0ebd5e7f0badffa8799fb84ea8dba0
Joshua <joshua43210@gmail.com>
2019-03-19 20:29:08 +0000

    add icon functionalities

--- web/script.js
+++ web/script.js
@@ -7,18 +7,18 @@ socket.on("server:website", (data) => {
   if (typeof hands !== "undefined") {
     let hand_type = hands["hand_type"];
     if (hand_type == "right") {
-      // Scale from [-300, 300] to [0, 600], then to [0, 800]
+      // Scale from [-300, 300] to [0, 600]
       // Canvas size is 400x400
-      let x = (parseInt(hands["hand_pos"]["x"]) + 300)/.75;
-      let z = (parseInt(hands["hand_pos"]["z"]) + 300)/.75;
+      let x = (parseInt(hands["hand_pos"]["x"]) + 300);
+      let z = (parseInt(hands["hand_pos"]["z"]) + 300);
       console.log(x, z);
       right_points.push([x, z]);
 
     } else if (hand_type== "left") {
-      // Scale from [-300, 300] to [0, 600], then to [0, 800]
+      // Scale from [-300, 300] to [0, 600]
       // Canvas size is 400x400
-      let x = (parseInt(hands["hand_pos"]["x"]) + 300)/.75;
-      let z = (parseInt(hands["hand_pos"]["z"]) + 300)/.75;
+      let x = (parseInt(hands["hand_pos"]["x"]) + 300);
+      let z = (parseInt(hands["hand_pos"]["z"]) + 300);
       console.log(x, z);
       left_points.push([x, z]);
     }
@@ -26,8 +26,9 @@ socket.on("server:website", (data) => {
 });
 
 var opaque_black;
+var canvas;
 function setup() {
-  var canvas = createCanvas(800, 800);
+  canvas = createCanvas(600, 600);
   canvas.parent("outer_frame");
 }
 
@@ -36,6 +37,7 @@ function draw() {
 
   strokeWeight(10);
   stroke(0, 0, 0, 50);
+
   for( let i = 1; i < right_points.length; i++) {
     // point(points[i][0], points[i][1]);
     line(right_points[i-1][0], right_points[i-1][1], right_points[i][0], right_points[i][1]);
@@ -47,4 +49,20 @@ function draw() {
 
 function stringCoords(ob) {
   return "(" + ob["hand_pos"]["x"] + ", " + ob["hand_pos"]["y"] + ", " + ob["hand_pos"]["z"] + ")";
+}
+$("#title").click(function() {
+  console.log("tittt");
+});
+
+$("#download").click(function () {
+  console.log("clicked");
+});
+
+function download() {
+  saveCanvas("BubblePicasso", "png");
+}
+
+function restart() {
+  right_points = [];
+  left_points = [];
 }
