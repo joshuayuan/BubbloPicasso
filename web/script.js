c1126c58b910e4ad8b6c2910611c0fc4e5b3fde5
Joshua <joshua43210@gmail.com>
2019-03-19 09:17:50 +0000

    asetheticize the canvas a bit

--- web/script.js
+++ web/script.js
@@ -4,23 +4,28 @@ var paths = [];
 socket.on("server:website", (data) => {
   $("#num_fingers").text(data["num_fingers"]);
   if (typeof data["hand_pos"] !== "undefined") {
-    $("#hand_pos").text(stringCoords(data));
-    let x = data["hand_pos"]["x"]/1.5;
-    let y = data["hand_pos"]["y"]/1.5;
-    console.log(x, y);
-    paths.push([x, y]);
-    console.log(paths);
+    $("#hand_pos_0").text(stringCoords(data));
+    // Scale from [-300, 300] to [0, 600], then to [0, 400]
+    // Canvas size is 400x400
+    let x = (parseInt(data["hand_pos"]["x"]) + 300)/1.5;
+    let z = (parseInt(data["hand_pos"]["z"]) + 300)/1.5;
+    console.log(x, z);
+    paths.push([x, z]);
   }
 });
 
+var opaque_black;
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(200);
+  background(255);
 
+  strokeWeight(10);
+  stroke(0, 0, 0, 50);
   for( let i = 1; i < paths.length; i++) {
+    // point(paths[i][0], paths[i][1]);
     line(paths[i-1][0], paths[i-1][1], paths[i][0], paths[i][1]);
   }
 }
