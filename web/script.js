6e2a21070ef1ac9cfa6c405fc85761e653a9e980
Joshua <joshua43210@gmail.com>
2019-03-20 06:48:17 +0000

    add dynamic line color and weight

--- web/script.js
+++ web/script.js
@@ -10,7 +10,7 @@ socket.on("server:website", (data) => {
       // Scale from [-300, 300] to [0, 600]
       // Canvas size is 400x400
       let x = (parseInt(hands["hand_pos"]["x"]) + 300); // side to side
-      let y = (parseInt(hands["hand_pos"]["y"]) + 300); // height
+      let y = -(parseInt(hands["hand_pos"]["y"]) - 700) / 20; // height
       let z = (parseInt(hands["hand_pos"]["z"]) + 300); // front to back
       right_points.push([x, y, z]);
 
@@ -18,7 +18,7 @@ socket.on("server:website", (data) => {
       // Scale from [-300, 300] to [0, 600]
       // Canvas size is 400x400
       let x = (parseInt(hands["hand_pos"]["x"]) + 300);
-      let y = (parseInt(hands["hand_pos"]["y"]) + 300); // height
+      let y = -(parseInt(hands["hand_pos"]["y"]) - 700)  / 20; // height
       let z = (parseInt(hands["hand_pos"]["z"]) + 300);
       left_points.push([x, y, z]);
     }
@@ -27,24 +27,30 @@ socket.on("server:website", (data) => {
 
 var opaque_black;
 var canvas;
+var hue = 0;
+
 function setup() {
   canvas = createCanvas(600, 600);
   canvas.parent("frame");
+  colorMode(HSB, 360, 100, 100, 100);
 }
 
-function draw() {
-  background(255);
 
-  strokeWeight(10);
-  stroke(0, 0, 0, 50);
+function draw() {
+  background(360);
+
 
   for( let i = 1; i < right_points.length; i++) {
     if (dist(right_points[i][0], right_points[i][2], right_points[i-1][0], right_points[i-1][2]) < 20) {
+      strokeWeight(right_points[i][1]);
+      stroke(i % 360, 75, 90);
       line(right_points[i-1][0], right_points[i-1][2], right_points[i][0], right_points[i][2]);
     }
   }
   for (let i = 1; i < left_points.length; i++ ) {
     if (dist(left_points[i][0], left_points[i][2], left_points[i-1][0], left_points[i-1][2]) < 20) {
+      strokeWeight(left_points[i][1]);
+      stroke(i % 360, 75, 90);
       line(left_points[i-1][0], left_points[i-1][2], left_points[i][0], left_points[i][2]);
     }
   }
