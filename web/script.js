71e24fe20c4dbaac66e7a9e2b0b33136d95f2970
Joshua <joshua43210@gmail.com>
2019-03-20 04:42:03 +0000

    polish css and remove jumpy lines

--- web/script.js
+++ web/script.js
@@ -9,18 +9,18 @@ socket.on("server:website", (data) => {
     if (hand_type == "right") {
       // Scale from [-300, 300] to [0, 600]
       // Canvas size is 400x400
-      let x = (parseInt(hands["hand_pos"]["x"]) + 300);
-      let z = (parseInt(hands["hand_pos"]["z"]) + 300);
-      console.log(x, z);
-      right_points.push([x, z]);
+      let x = (parseInt(hands["hand_pos"]["x"]) + 300); // side to side
+      let y = (parseInt(hands["hand_pos"]["y"]) + 300); // height
+      let z = (parseInt(hands["hand_pos"]["z"]) + 300); // front to back
+      right_points.push([x, y, z]);
 
     } else if (hand_type== "left") {
       // Scale from [-300, 300] to [0, 600]
       // Canvas size is 400x400
       let x = (parseInt(hands["hand_pos"]["x"]) + 300);
+      let y = (parseInt(hands["hand_pos"]["y"]) + 300); // height
       let z = (parseInt(hands["hand_pos"]["z"]) + 300);
-      console.log(x, z);
-      left_points.push([x, z]);
+      left_points.push([x, y, z]);
     }
   }
 });
@@ -29,27 +29,31 @@ var opaque_black;
 var canvas;
 function setup() {
   canvas = createCanvas(600, 600);
-  canvas.parent("frame_container");
+  canvas.parent("frame");
 }
 
 function draw() {
-  background(250);
+  background(255);
 
   strokeWeight(10);
   stroke(0, 0, 0, 50);
 
   for( let i = 1; i < right_points.length; i++) {
-    // point(points[i][0], points[i][1]);
-    line(right_points[i-1][0], right_points[i-1][1], right_points[i][0], right_points[i][1]);
+    if (dist(right_points[i][0], right_points[i][2], right_points[i-1][0], right_points[i-1][2]) < 20) {
+      line(right_points[i-1][0], right_points[i-1][2], right_points[i][0], right_points[i][2]);
+    }
   }
   for (let i = 1; i < left_points.length; i++ ) {
-    line(left_points[i-1][0], left_points[i-1][1], left_points[i][0], left_points[i][1]);
+    if (dist(left_points[i][0], left_points[i][2], left_points[i-1][0], left_points[i-1][2]) < 20) {
+      line(left_points[i-1][0], left_points[i-1][2], left_points[i][0], left_points[i][2]);
+    }
   }
 }
 
 function stringCoords(ob) {
   return "(" + ob["hand_pos"]["x"] + ", " + ob["hand_pos"]["y"] + ", " + ob["hand_pos"]["z"] + ")";
 }
+
 $("#title").click(function() {
   console.log("tittt");
 });
